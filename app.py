a4e8d2e31f2392bbd475c57df9a2796e29f97f77
sid9773 <sidsharma9773@gmail.com>
2021-04-27 18:03:28 +0000

    messi details

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def index():
 @app.get('/barcelona')
 def index():
     return {'data': 'Messi, Fati, Griezman, Lenglet, Umtiti, Alba'}
+
+@app.get('/barcelona/messi')
+def index():
+    return {'data': 'Name:Lionel Messi, Age:33,Nationality:Argentina'}
