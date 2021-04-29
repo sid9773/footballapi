5141f2d4c39f994093703409fe832ee35d6f0efb
sid9773 <sidsharma9773@gmail.com>
2021-04-29 17:46:24 +0000

    alba pique

--- app.py
+++ app.py
@@ -13,3 +13,11 @@ def index():
 @app.get('/barcelona/messi')
 def index():
     return {'data': 'Name:Lionel Messi, Age:33,Nationality:Argentina'}
+
+@app.get('/barcelona/alba')
+def index():
+    return {'data': 'Name:Jordi Alba, Age:30,Nationality:Spain'}
+
+@app.get('/barcelona/pique')
+def index():
+    return {'data': 'Name:Gerard Pique, Age:31,Nationality:Spain'}
